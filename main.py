e8073ad9933803c935f98189a2e2857610c8a5f0
Gagan <business@devgagan.in>
2025-03-19 10:58:49 +0530

    Fixed some time starting problem and loop problems

--- main.py
+++ main.py
@@ -3,16 +3,14 @@
 # See LICENSE file in the repository root for full license text.
 
 import asyncio
-from shared_client import start_client
 import importlib
 import os
 import sys
-from pyrogram.types import User, Chat
+from shared_client import start_client
+
 
 async def load_and_run_plugins():
-    # Start the shared client
-    client, app, userbot = await start_client()
-    
+    await start_client()
     plugin_dir = "plugins"
     plugins = [f[:-3] for f in os.listdir(plugin_dir) if f.endswith(".py") and f != "__init__.py"]
 
@@ -20,22 +18,26 @@ async def load_and_run_plugins():
         module = importlib.import_module(f"plugins.{plugin}")
         if hasattr(module, f"run_{plugin}_plugin"):
             print(f"Running {plugin} plugin...")
-            await getattr(module, f"run_{plugin}_plugin")()
+            await getattr(module, f"run_{plugin}_plugin")() 
+
 
 async def main():
     await load_and_run_plugins()
-    while True:
-        await asyncio.sleep(1)
+
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
     print("Starting clients ...")
+
+    loop = asyncio.new_event_loop()  # Fix kr diya
+    asyncio.set_event_loop(loop)
+
     try:
         loop.run_until_complete(main())
+        loop.run_forever()  # now run forever directly 
     except KeyboardInterrupt:
         print("Shutting down...")
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
         sys.exit(1)
     finally:
-        loop.close()
+        loop.stop()  # console me error nhi bharega
